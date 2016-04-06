5b57d433aba24c95e429c45a0922843e135cc8c2
shawnsong <xys8628@gmail.com>
2016-04-06 17:23:28 +0800

    longest common prefix amongst an array of strings

---
+++ LeetCode/src/leetcode/Q14_LongestCommonPrefix.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+/*
+ * Write a function to find the longest common prefix string amongst an array of strings. 
+ */
+public class Q14_LongestCommonPrefix {
+	public String longestCommonPrefix(String[] strs) {
+		if (strs == null || strs.length == 0)
+			return "";
+
+		if (strs.length == 1)
+			return strs[0];
+
+		int minLength = Integer.MAX_VALUE;
+		for (int i = 0; i < strs.length; i++) {
+			if (strs[i].length() == 0)
+				return "";
+			else if (strs[i].length() < minLength)
+				minLength = strs[i].length();
+		}
+
+		for (int j = 0; j < minLength; j++) {
+			for (int i = 1; i < strs.length; i++) {
+				if (strs[0].charAt(j) != strs[i].charAt(j))
+					return strs[0].substring(0, j);
+			}
+		}
+		return strs[0].substring(0, minLength);
+	}
+}
