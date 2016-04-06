798ed30d5d0e57f24bbc73213d4e6650bb2e2440
Shawn Song <xys8628@gmail.com>
2016-04-06 23:45:41 +0800

    136

---
+++ LeetCode/src/leetcode/Q136_SingleNumber.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+/*
+ * Given an array of integers, every element appears twice except for one. Find that single one.
+ */
+public class Q136_SingleNumber {
+	public int singleNumber(int[] nums) {
+		int num = 0;
+		for (int i = 0; i < nums.length; i++) {
+			num = num ^ nums[i];
+		}
+		return num;
+	}
+}
