ae0e3c0a104ea89607ecc8f1f6520dbdacb9157f
shawnsong <xys8628@gmail.com>
2016-04-20 10:53:11 +0800

    fix the bug that return value is 1 when matrix is all zeros

--- LeetCode/src/leetcode/Q221_MaximalSquare.java
+++ LeetCode/src/leetcode/Q221_MaximalSquare.java
@@ -21,15 +21,19 @@ public class Q221_MaximalSquare {
 		int width = matrix[0].length;
 		
 		int[][] dp = new int[height][width];
-		
+		int max = 0;
 		for (int i = 0; i < height; i++) {
 			dp[i][0] = matrix[i][0] - '0';
+			if (matrix[i][0] == '1')
+				max = 1;
 		}
 		for (int i = 0; i < width; i++) {
 			dp[0][i] = matrix[0][i] - '0';
+			if (matrix[0][i] == '1')
+				max = 1;
 		}
 		
-		int max = 1;
+		
 		for (int i = 1; i< height; i++) {
 			for (int j = 1; j < width; j++) {
 				if (matrix[i][j] == '1') {
