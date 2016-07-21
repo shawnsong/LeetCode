db34a71712f40a0433d0e0742fa7a8b050e2fc45
Shawn Song <xys8628@gmail.com>
2016-07-21 22:16:45 +0800

    fixed bugs. added comments

--- LeetCode/src/leetcode/Q152_MaximumProductSubarray.java
+++ LeetCode/src/leetcode/Q152_MaximumProductSubarray.java
@@ -1,23 +1,30 @@
 package leetcode;
 
+/*
+ Find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+ For example, given the array [2,3,-2,4],
+ the contiguous subarray [2,3] has the largest product = 6.
+
+
+ */
 public class Q152_MaximumProductSubarray {
 
 	public int maxProduct(int[] nums) {
 		if (nums == null || nums.length == 0)
 			return 0;
-		
+
 		int max = nums[0];
 		int min = nums[0];
 		int res = nums[0];
-		
-		for (int i = 0; i < nums.length; i++) {
-			int tmp = max;
+
+		for (int i = 1; i < nums.length; i++) {
 			max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
 			min = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
-			
+
 			res = Math.max(max, res);
 		}
-		
+
 		return res;
 	}
 }
