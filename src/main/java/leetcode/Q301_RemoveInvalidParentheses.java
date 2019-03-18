87180b7e9a616606f966f1788507708ba9efb5c0
Shawn Song <mr.song.shawn@gmail.com>
2019-03-18 15:47:07 +1100

    .

---
+++ src/main/java/leetcode/Q301_RemoveInvalidParentheses.java
@@ -0,0 +1,4 @@
+package leetcode;
+
+public class Q301_RemoveInvalidParentheses {
+}
---
+++ src/test/java/leetcode/Q301_RemoveInvalidParenthesesTest.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Q301_RemoveInvalidParenthesesTest {
+
+    Q301_RemoveInvalidParentheses test = new Q301_RemoveInvalidParentheses();
+
+    @Test
+    void removeInvalidParentheses() {
+        System.out.println(test.removeInvalidParentheses("()())())"));
+    }
+}
