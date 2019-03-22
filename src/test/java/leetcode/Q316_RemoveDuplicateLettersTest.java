b1d19889730022980ac4b0ed3739fa036d71fc45
Shawn Song <mr.song.shawn@gmail.com>
2019-03-22 21:47:55 +1100

    .

---
+++ src/test/java/leetcode/Q316_RemoveDuplicateLettersTest.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Q316_RemoveDuplicateLettersTest {
+
+    Q316_RemoveDuplicateLetters test = new Q316_RemoveDuplicateLetters();
+    @Test
+    void test1() {
+        System.out.println(test.removeDuplicateLetters1("baab"));
+    }
+}
