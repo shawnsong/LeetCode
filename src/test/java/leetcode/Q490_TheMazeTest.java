b82fd3f7627e9e9b92dd4fe9dbd8aca3cfb2b8c8
Shawn Song <mr.song.shawn@gmail.com>
2019-04-16 16:12:19 +1000

    .

--- src/test/java/leetcode/Q490_TheMazeTest.java
+++ src/test/java/leetcode/Q490_TheMazeTest.java
@@ -6,7 +6,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Q490_TheMazeTest {
 
+    Q490_TheMaze test = new Q490_TheMaze();
+
     @Test
     void hasPath() {
+        int[][] maze = {
+            {0,0,1,0,0},
+            {0,0,0,0,0},
+            {0,0,0,1,0},
+            {1,1,0,1,1},
+            {0,0,0,0,0}
+        };
+        int[] start = {0,4} ;
+        int[] end = {4,4};
+
+        test.hasPath(maze, start,end);
+
+        String data = "#,1,,";
+        String[] vals = data.split(",");
+        System.out.println(vals.length);
     }
 }
