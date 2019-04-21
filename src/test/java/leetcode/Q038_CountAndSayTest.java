68cd6ccad0266ef43aff46704971bfc398385b01
Shawn Song <mr.song.shawn@gmail.com>
2019-04-21 19:39:26 +0800

    .

--- src/test/java/leetcode/Q038_CountAndSayTest.java
+++ src/test/java/leetcode/Q038_CountAndSayTest.java
@@ -6,11 +6,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Q038_CountAndSayTest {
 
+    Q038_CountAndSay test = new Q038_CountAndSay();
+
     @Test
     void countAndSay() {
     }
 
     @Test
     void generateNext() {
+        assertEquals("11", test.generateNext("1"));
+        assertEquals("21", test.generateNext("11"));
+        assertEquals("1211", test.generateNext("21"));
     }
 }
