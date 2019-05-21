bb3158517856f1890456e28409c189506aad21fe
Shawn Song <mr.song.shawn@gmail.com>
2019-05-21 21:12:43 +1000

    fixed 662

--- src/main/java/leetcode/Q662_MaxTreeWidth.java
+++ src/main/java/leetcode/Q662_MaxTreeWidth.java
@@ -4,27 +4,26 @@ import java.util.ArrayList;
 
 public class Q662_MaxTreeWidth {
     public int widthOfBinaryTree(TreeNode root) {
-        return traverse(root, 0, 0, new ArrayList(), new ArrayList());
+        return traverse(root, 0, 0, new ArrayList<>(), new ArrayList<>());
     }
 
-    int traverse(TreeNode root, int level, int order, ArrayList<Integer> start, ArrayList<Integer> end) {
-        if (root == null) return 0;
+    private int traverse(TreeNode node, int level, int order, ArrayList<Integer> starts, ArrayList<Integer> ends) {
+        if (node == null) return 0;
 
-        if (level == start.size()) {
-            start.add(level);
-            end.add(level);
+        if (level == starts.size()) {
+            starts.add(order);
+            ends.add(order);
         } else {
-            if (order < start.get(level)) {
-                start.set(level, order);
+            if (order < starts.get(level)) {
+                starts.set(level, order);
             }
-            if (order > end.get(level)) {
-                end.set(level, order);
+            if (order > ends.get(level)) {
+                ends.set(level, order);
             }
         }
-        int cur = end.get(level) - start.get(level) + 1;
-
-        int left = traverse(root.left, level + 1, order * 2 + 1, start, end);
-        int right = traverse(root.right, level+1, order * 2 + 2, start, end);
+        int cur = ends.get(level) - starts.get(level) + 1;
+        int left = traverse(node.left, level + 1, order * 2 + 1, starts, ends);
+        int right = traverse(node.right, level + 1, order * 2 + 2, starts, ends);
 
         return Math.max(cur, Math.max(left, right));
     }
