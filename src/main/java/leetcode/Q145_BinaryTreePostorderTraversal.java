713c14c273615cfc44b233330e89ed0ed46265af
Shawn Song <mr.song.shawn@gmail.com>
2019-05-26 22:49:55 +1000

    .

--- src/main/java/leetcode/Q145_BinaryTreePostorderTraversal.java
+++ src/main/java/leetcode/Q145_BinaryTreePostorderTraversal.java
@@ -8,17 +8,40 @@ public class Q145_BinaryTreePostorderTraversal {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> res = new LinkedList<>();
 
+        if (root == null) return res;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            root = stack.pop();
+            res.add(0, root.val);
+            if (root.right != null) {
+                stack.push(root.right);
+            }
+            if (root.left != null) {
+                stack.push(root.left);
+            }
+        }
+
+        return res;
+    }
+
+    public List<Integer> postorderTraversal2(TreeNode root) {
+        List<Integer> res = new LinkedList<>();
+
+        if (root == null) return res;
+
         Stack<TreeNode> stack = new Stack<>();
         while (root != null || !stack.isEmpty()) {
             while (root != null) {
-                res.add(0, root.val);
                 stack.push(root);
                 root = root.right;
             }
             root = stack.pop();
+            res.add(0, root.val);
             root = root.left;
         }
-
         return res;
     }
 }
