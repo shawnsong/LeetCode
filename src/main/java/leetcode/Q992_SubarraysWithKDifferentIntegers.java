32d84731ea06afb1af938042c5a1fc9a742c81b2
Shawn Song <mr.song.shawn@gmail.com>
2019-05-29 22:58:47 +1000

    992

--- src/main/java/leetcode/Q992_SubarraysWithKDifferentIntegers.java
+++ src/main/java/leetcode/Q992_SubarraysWithKDifferentIntegers.java
@@ -1,4 +1,53 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array A of positive integers, call a (contiguous, not necessarily distinct) subarray of A good
+ * if the number of different integers in that subarray is exactly K.
+ *
+ * (For example, [1,2,3,1,2] has 3 different integers: 1, 2, and 3.)
+ *
+ * Return the number of good subarrays of A.
+
+ * Example 1:
+ * Input: A = [1,2,1,2,3], K = 2
+ * Output: 7
+ * Explanation: Subarrays formed with exactly 2 different integers: [1,2], [2,1], [1,2], [2,3], [1,2,1], [2,1,2], [1,2,1,2].
+ *
+ * Example 2:
+ * Input: A = [1,2,1,3,4], K = 3
+ * Output: 3
+ * Explanation: Subarrays formed with exactly 3 different integers: [1,2,1,3], [2,1,3], [1,3,4].
+ */
 public class Q992_SubarraysWithKDifferentIntegers {
+
+    public int subarraysWithKDistinct(int[] A, int K) {
+        return atMostK(A, K) - atMostK(A, K - 1);
+    }
+
+    int atMostK(int[] A, int K) {
+        int count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int left = 0;
+        for (int i = 0; i < A.length; i++) {
+            map.put(A[i], map.getOrDefault(A[i], 0) + 1);
+            while (map.size() > K ) {
+                int num = A[left];
+                map.put(num, map.get(num) - 1);
+                if (map.get(num) == 0) {
+                    map.remove(num);
+                }
+                left++;
+            }
+            // 为什么有 count += i - left + 1?
+            // 因为subarray是从left 到i并且最多有K个数字, 那么连续的subarray就是
+            // (left...i), (left+1...i), (left+2...i) .... (i-1...i), (i...i) 一共是i - left + 1个数组
+            count += i - left + 1;
+        }
+        return count;
+    }
+
 }
--- src/test/java/leetcode/Q992_SubarraysWithKDifferentIntegersTest.java
+++ src/test/java/leetcode/Q992_SubarraysWithKDifferentIntegersTest.java
@@ -1,5 +1,15 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Q992_SubarraysWithKDifferentIntegersTest {
 
+    Q992_SubarraysWithKDifferentIntegers test = new Q992_SubarraysWithKDifferentIntegers();
+
+    @Test
+    void atMostK() {
+        assertEquals(7, test.subarraysWithKDistinct(new int[] {1,2,1,2,3}, 2));
+    }
 }
