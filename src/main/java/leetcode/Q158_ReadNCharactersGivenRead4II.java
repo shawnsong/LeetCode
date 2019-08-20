33af230bbb5abadf2f1143cedce3af1b2b9bd202
Shawn Song <mr.song.shawn@gmail.com>
2019-08-20 18:29:28 +1000

    read4II

--- src/main/java/leetcode/Q158_ReadNCharactersGivenRead4II.java
+++ src/main/java/leetcode/Q158_ReadNCharactersGivenRead4II.java
@@ -1,4 +1,35 @@
 package leetcode;
 
-public class Q158_ReadNCharactersGivenRead4II {
+public class Q158_ReadNCharactersGivenRead4II implements Read4 {
+    char[] buff = new char[4];
+    int pos = 0;
+    int size = 0;
+
+    public int read(char[] buf, int n) {
+        int count = 0;
+        while (count < n) {
+            if (pos == size) {
+                pos = 0;
+                size = read4(buff);
+                if (size == 0) {
+                    break;
+                }
+            }
+            while (count < n && pos < size) {
+                buf[count] = buff[pos];
+                count++;
+                pos++;
+            }
+        }
+        return count;
+    }
+
+    @Override
+    public int read4(char[] buff) {
+        return 0;
+    }
+}
+
+interface Read4 {
+    int read4(char[] buff);
 }
