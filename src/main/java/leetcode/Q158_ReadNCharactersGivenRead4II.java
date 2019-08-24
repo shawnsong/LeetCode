2f2630f633ca51cf8b6b7765c963b2bdefd4b4a4
Shawn Song <mr.song.shawn@gmail.com>
2019-08-24 21:45:17 +1000

    read4 II

--- src/main/java/leetcode/Q158_ReadNCharactersGivenRead4II.java
+++ src/main/java/leetcode/Q158_ReadNCharactersGivenRead4II.java
@@ -1,4 +1,40 @@
 package leetcode;
 
+import java.util.Random;
+
+/**
+ * Given a file and assume that you can only read the file using a given method read4, implement a method read to read n characters.
+ * Your method read may be called multiple times.
+ */
 public class Q158_ReadNCharactersGivenRead4II {
+
+    private char[] buff;
+    private int buffCount;
+    private int buffPos;
+
+    public int read(char[] buf, int n) {
+        int pos = 0;
+        while (pos < n) {
+            if (buffPos == 0) {
+                buffCount = read4(buff);
+                if (buffCount == 0) {
+                    break;
+                }
+            }
+
+            while (pos < n && buffPos < buffCount) {
+                buf[pos] = buff[buffPos];
+                pos++;
+                buffPos++;
+            }
+            if (buffPos == buffCount) {
+                buffPos = 0;
+            }
+        }
+        return pos;
+    }
+
+    private int read4(char[] buf) {
+        return new Random().nextInt(4);
+    }
 }
