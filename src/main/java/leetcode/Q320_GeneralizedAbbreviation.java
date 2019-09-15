5a0f236c29e35f3e7834a0ac66114a5efe1abd28
Shawn Song <mr.song.shawn@gmail.com>
2019-09-15 20:36:05 +1000

    320

--- src/main/java/leetcode/Q320_GeneralizedAbbreviation.java
+++ src/main/java/leetcode/Q320_GeneralizedAbbreviation.java
@@ -3,6 +3,14 @@ package leetcode;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Write a function to generate the generalized abbreviations of a word.
+ *
+ * Example:
+ * Input: "word"
+ * Output:
+ * ["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4"]
+ */
 public class Q320_GeneralizedAbbreviation {
     public List<String> generateAbbreviations(String word){
         List<String> ret = new ArrayList<String>();
