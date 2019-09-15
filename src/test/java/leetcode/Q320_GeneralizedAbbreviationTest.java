3d44b74e989f5069b28e5024aa020c8e1e24c706
Shawn Song <mr.song.shawn@gmail.com>
2019-09-15 20:34:36 +1000

    320

---
+++ src/test/java/leetcode/Q320_GeneralizedAbbreviationTest.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Q320_GeneralizedAbbreviationTest {
+    Q320_GeneralizedAbbreviation test = new Q320_GeneralizedAbbreviation();
+
+    @Test
+    void generateAbbreviations() {
+        System.out.println(test.generateAbbreviations("ab"));
+    }
+}
