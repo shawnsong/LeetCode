87fd69216e6908c5f3161ef684f5aeaf65c758bb
Shawn Song <mr.song.shawn@gmail.com>
2019-10-14 23:08:39 +1100

    772

--- src/main/java/leetcode/Q772_BasicCalculator.java
+++ src/main/java/leetcode/Q772_BasicCalculator.java
@@ -2,49 +2,52 @@ package leetcode;
 
 public class Q772_BasicCalculator {
     public int calculate(String s) {
-        int lowVal = 0;
-        char lowSign = '+';
-
-        int highVal = 1;
-        char highSign = '*';
-
-        for (int i = 0; i < s.length(); i++) {
+        int o1 = 1, l1 = 0;
+        int o2 = 1, l2 = 1;
+        s = s.trim();
+        int i = 0;
+        if (s.charAt(i) == '-' || s.charAt(i) == '+') {
+            o1 = s.charAt(i) == '+' ? 1 : -1;
+            i++;
+        }
+        for (; i < s.length(); i++) {
             char c = s.charAt(i);
             if (Character.isDigit(c)) {
                 int num = 0;
                 while (i < s.length() && Character.isDigit(s.charAt(i))) {
-                    int digit = s.charAt(i) - '0';
-                    num = num * 10 + digit;
+                    num = num * 10 + (s.charAt(i) - '0');
                     i++;
                 }
                 i--;
-                highVal = highSign == '*' ? highVal * num : highVal / num;
+                l2 = (o2 == 1 ? l2 * num : l2 / num);
             } else if (c == '(') {
-                int count = 0;
-                int j = i;
-                for (; i < s.length(); i++) {
+                int left = 0;
+                int start = i;
+                while (i < s.length()) {
                     if (s.charAt(i) == '(') {
-                        count++;
-                    } else if (s.charAt(i) == ')') {
-                        count--;
+                        left++;
                     }
-                    if (count == 0) {
+                    if (s.charAt(i) == ')') {
+                        left--;
+                    }
+                    if (left == 0) {
                         break;
                     }
+                    i++;
                 }
-                String sub = s.substring(j+1, i);
-                int subRes = calculate(sub);
-                highVal = highSign == '*' ? highVal * subRes : highVal / subRes;
+                String substr = s.substring(start + 1, i);
+                int subRes = calculate(substr);
+                l2 = (o2 == 1 ? l2 * subRes : l2 / subRes);
             } else if (c == '+' || c == '-') {
-                lowVal = lowVal + (lowSign == '+' ? highVal : -highVal);
-                lowSign = c;
-
-                highVal = 1;
-                highSign = '*';
+                l1 = l1 + o1 * l2;
+                l2 = 1;
+                o2 = 1;
+                o1 = c == '+' ? 1 : -1;
             } else if (c == '*' || c == '/') {
-                highSign = c;
+                o2 = c == '*' ? 1 : -1;
             }
         }
-        return lowVal + (lowSign == '+' ? highVal : -highVal);
+        l1 = l1 + o1 * l2;
+        return l1;
     }
 }
