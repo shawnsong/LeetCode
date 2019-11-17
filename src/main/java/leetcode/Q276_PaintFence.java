70ec6a67af16ed4f2f8d3f075b891d0cb1391872
Shawn Song <mr.song.shawn@gmail.com>
2019-11-17 13:01:41 +1100

    comments

--- src/main/java/leetcode/Q276_PaintFence.java
+++ src/main/java/leetcode/Q276_PaintFence.java
@@ -1,5 +1,12 @@
 package leetcode;
 
+/**
+ * There is a fence with n posts, each post can be painted with one of the k colors.
+ *
+ * You have to paint all the posts such that no more than two adjacent fence posts have the same color.
+ *
+ * Return the total number of ways you can paint the fence.
+ */
 public class Q276_PaintFence {
     public int numWays(int n, int k) {
         if (n == 0) {
