204849d1017b0a5cbd1ec3b17beefddc602ddde0
Shawn Song <mr.song.shawn@gmail.com>
2019-11-23 00:19:43 +1100

    416 0/1 knapsack

---
+++ src/main/java/leetcode/Q416_PartitionEqualSubsetSum.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+public class Q416_PartitionEqualSubsetSum {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 == 1) {
+            return false;
+        }
+        sum = sum / 2;
+
+        int m = nums.length;
+        boolean[][] dp = new boolean[m + 1][sum + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = true;
+        }
+        for (int i = 1; i <= sum; i++) {
+            dp[0][i] = false;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= sum; j++) {
+                dp[i][j] = dp[i-1][j];
+                if (j >= nums[i-1]) {
+                    dp[i][j] = dp[i][j] || dp[i-1][j - nums[i-1]];
+                }
+            }
+        }
+        return dp[m][sum];
+    }
+}
