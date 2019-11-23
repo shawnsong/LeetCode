a0f4bd65d78f761dbb808c434d82cd6158c9e66b
Shawn Song <mr.song.shawn@gmail.com>
2019-11-23 22:24:53 +1100

    rename to reservoir sampling

--- src/main/java/leetcode/Q382_ReservoirSampling.java
+++ src/main/java/leetcode/Q382_ReservoirSampling.java
@@ -2,12 +2,12 @@ package leetcode;
 
 import java.util.Random;
 
-public class Q382_Sub {
+public class Q382_ReservoirSampling {
     ListNode head;
     Random rand;
     int k;
 
-    public Q382_Sub(ListNode head, int k) {
+    public Q382_ReservoirSampling(ListNode head, int k) {
         this.head = head;
         rand = new Random();
         this.k = k;
