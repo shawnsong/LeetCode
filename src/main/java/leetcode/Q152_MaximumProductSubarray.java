8bedfca12e684d1b80c2184963a619513ebeb85d
Shawn Song <mr.song.shawn@gmail.com>
2019-11-25 22:29:54 +1100

    152

--- src/main/java/leetcode/Q152_MaximumProductSubarray.java
+++ src/main/java/leetcode/Q152_MaximumProductSubarray.java
@@ -20,7 +20,7 @@ public class Q152_MaximumProductSubarray {
 
 		for (int i = 1; i < nums.length; i++) {
 			max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
-			min = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
+			min = Math.min(Math.min(max * nums[i], min * nums[i]), nums[i]);
 
 			res = Math.max(max, res);
 		}
---
+++ src/test/java/leetcode/Q152_MaximumProductSubarrayTest.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Q152_MaximumProductSubarrayTest {
+
+    @Test
+    void maxProduct() {
+        Q152_MaximumProductSubarray test = new Q152_MaximumProductSubarray();
+        test.maxProduct(new int[] {-4, -3, -2});
+    }
+}
