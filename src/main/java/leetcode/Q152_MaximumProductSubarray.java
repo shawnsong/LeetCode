097f0af1baf07d8a4c13c6de7d07c345c595391b
Shawn Song <mr.song.shawn@gmail.com>
2019-11-25 22:40:45 +1100

    152

--- src/main/java/leetcode/Q152_MaximumProductSubarray.java
+++ src/main/java/leetcode/Q152_MaximumProductSubarray.java
@@ -1,30 +1,24 @@
 package leetcode;
 
-/*
- Find the contiguous subarray within an array (containing at least one number) which has the largest product.
-
- For example, given the array [2,3,-2,4],
- the contiguous subarray [2,3] has the largest product = 6.
-
-
+/**
+ * Find the contiguous subarray within an array (containing at least one number) which has the largest product.
+ * For example, given the array [2,3,-2,4],
+ * the contiguous subarray [2,3] has the largest product = 6.
  */
 public class Q152_MaximumProductSubarray {
 
 	public int maxProduct(int[] nums) {
-		if (nums == null || nums.length == 0)
-			return 0;
+        int prevMax = 1;
+        int prevMin = 1;
+        int res = nums[0];
 
-		int max = nums[0];
-		int min = nums[0];
-		int res = nums[0];
-
-		for (int i = 1; i < nums.length; i++) {
-			max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
-			min = Math.min(Math.min(max * nums[i], min * nums[i]), nums[i]);
-
-			res = Math.max(max, res);
-		}
-
-		return res;
+        for (int num : nums) {
+            int max = Math.max(num, Math.max(prevMax * num, prevMin * num));
+            int min = Math.min(num, Math.min(prevMax * num, prevMin * num));
+            res = Math.max(res, max);
+            prevMax = max;
+            prevMin = min;
+        }
+        return res;
 	}
 }
--- src/test/java/leetcode/Q152_MaximumProductSubarrayTest.java
+++ src/test/java/leetcode/Q152_MaximumProductSubarrayTest.java
@@ -9,6 +9,7 @@ class Q152_MaximumProductSubarrayTest {
     @Test
     void maxProduct() {
         Q152_MaximumProductSubarray test = new Q152_MaximumProductSubarray();
-        test.maxProduct(new int[] {-4, -3, -2});
+        assertEquals(12, test.maxProduct(new int[] {-4, -3, -2}));
+        assertEquals(12, test.maxProduct(new int[] {-3, -2, 0, 3, 4}));
     }
 }
