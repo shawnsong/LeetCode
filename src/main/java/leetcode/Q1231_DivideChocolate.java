c675d1c7346d5a148caed00c613ef117943f654b
Shawn Song <mr.song.shawn@gmail.com>
2019-12-01 00:51:17 +1100

    added comments

--- src/main/java/leetcode/Q1231_DivideChocolate.java
+++ src/main/java/leetcode/Q1231_DivideChocolate.java
@@ -1,5 +1,11 @@
 package leetcode;
 
+/**
+ * divide the array to k + 1 parts, each part is a continuous sub-array of the array. find the largest possible sum
+ * of the smallest sub-arrays, i.e. we want the smallest sub-array to be as large as possible.
+ *
+ * Similar to Q1101 & Q401
+ */
 public class Q1231_DivideChocolate {
     public int maximizeSweetness(int[] sweetness, int k) {
         int left = 1, right = 1000000000 / (k + 1);
