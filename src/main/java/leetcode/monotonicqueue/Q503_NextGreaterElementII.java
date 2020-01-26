849352c53cbd2a57f21bb9c692568f609a97e6a0
Shawn Song <mr.song.shawn@gmail.com>
2020-01-26 17:48:03 +1100

    monotonic queue

--- src/main/java/leetcode/monotonicqueue/Q503_NextGreaterElementII.java
+++ src/main/java/leetcode/monotonicqueue/Q503_NextGreaterElementII.java
@@ -1,16 +1,18 @@
 package leetcode.monotonicqueue;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class Q503_NextGreaterElementII {
     public int[] nextGreaterElements(int[] nums) {
         Stack<Integer> stack = new Stack<>();
         int[] res = new int[nums.length];
+        Arrays.fill(res, -1);
         for (int i = 0; i < nums.length; i++) {
             while (!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
                 res[stack.pop()] = nums[i];
             }
-            stack.push(nums[i]);
+            stack.push(i);
         }
         for (int i = 0; i < nums.length; i++) {
             while (!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
